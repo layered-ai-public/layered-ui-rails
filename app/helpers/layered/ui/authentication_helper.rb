ad61e02cf398a43d28642c4c4ad500b20f0cd2a1
Tom Gallagher <tom.gallagher@theappengineers.com>
2026-08-14 18:11:56 +0100

    Make Devise auth links scope-agnostic (#111)
    Add Layered::Ui.devise_scope config so header/sidebar login, register,
    and logout links work with non-default Devise mappings (e.g. devise_for
    :admins), instead of assuming :user.

--- app/helpers/layered/ui/authentication_helper.rb
+++ app/helpers/layered/ui/authentication_helper.rb
@@ -13,6 +13,25 @@ module Layered
       def l_ui_user_signed_in?
         l_ui_current_user.present?
       end
+
+      def l_ui_new_registration_path
+        l_ui_devise_path(:new, :registration)
+      end
+
+      def l_ui_new_session_path
+        l_ui_devise_path(:new, :session)
+      end
+
+      def l_ui_destroy_session_path
+        l_ui_devise_path(:destroy, :session)
+      end
+
+      private
+
+      def l_ui_devise_path(prefix, resource)
+        method = :"#{prefix}_#{Layered::Ui.devise_scope}_#{resource}_path"
+        main_app.public_send(method) if main_app.respond_to?(method)
+      end
     end
   end
 end
--- lib/layered/ui.rb
+++ lib/layered/ui.rb
@@ -5,5 +5,6 @@ require "layered/ui/engine"
 module Layered
   module Ui
     mattr_accessor :current_user_method, default: :current_user
+    mattr_accessor :devise_scope, default: :user
   end
 end
